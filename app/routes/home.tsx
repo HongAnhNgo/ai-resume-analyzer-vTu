1c07507f1a9d19606237425bbf6af258adb183ec
Hong Anh Ngo <hatl10603@gmail.com>
2026-06-24 23:00:35 +0700

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,31 +2,45 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "IResume - the Resume Analyzer" },
-    { name: "description", content: "Analyze your resume with AI-powered insights." },
+    {
+      name: "description",
+      content: "Analyze your resume with AI-powered insights.",
+    },
   ];
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    <section className="main-section">
-      <div className="page-heading">
-        <h1>Track Your Applications & Resume Ratings</h1>
-        <h2>Get instant feedback on your resume with AI-powered insights.</h2>
-      </div>
-    </section>
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
 
-    {resumes.length > 0 && (
-      <section className="resume-section">
-        {resumes.map((resume) => (
-          <ResumeCard key={resume.id} resume={resume} />
-        ))}
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("auth?next=/");
+  }, [auth.isAuthenticated]);
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading p-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Get instant feedback on your resume with AI-powered insights.</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <section className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </section>
+        )}
       </section>
-    )}
-   
-  </main>;
+    </main>
+  );
 }
